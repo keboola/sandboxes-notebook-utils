f5fc98a7d7436c39b6f22155507723863e568f12
Erik Zigo <erik.zigo@keboola.com>
2026-02-09 14:58:33 +0100

    chore: upgrade minimum python version from 3.8 to 3.10

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     url='https://github.com/keboola/sandboxes-notebook-utils',
     packages=['keboola_notebook_utils'],
     package_dir={'keboola_notebook_utils': ''},
-    requires=['pip']
+    python_requires='>=3.10',
+    install_requires=['requests'],
 )
