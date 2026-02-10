8089378fc119b215ee4ed0afd3812847bc5962bb
Erik Zigo <erik.zigo@keboola.com>
2026-02-10 11:10:10 +0100

    chore: bump version to 2.3.0.dev2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='keboola-sandboxes-notebook-utils',
-    version='2.2.0.dev1',
+    version='2.3.0.dev2',
     url='https://github.com/keboola/sandboxes-notebook-utils',
     packages=['keboola_notebook_utils'],
     package_dir={'keboola_notebook_utils': ''},
