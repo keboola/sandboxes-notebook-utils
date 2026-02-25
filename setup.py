e092524b1f3a142d6fa4fe4013ac858fe0408ea4
Erik Zigo <erik.zigo@keboola.com>
2026-02-25 17:32:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='keboola-sandboxes-notebook-utils',
-    version='1.4.0.dev1',
+    version='1.4.0,
     url='https://github.com/keboola/sandboxes-notebook-utils',
     packages=['keboola_notebook_utils'],
     package_dir={'keboola_notebook_utils': ''},
